0d2d525cb97389016130954a1970695bcfeb890f
CaioCresencio <caiocresencio98@gmail.com>
2019-11-27 09:26:24 -0300

    Criação do projeto

---
+++ eol/src/main/java/br/com/ifsp/eol/EolApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifsp.eol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EolApplication.class, args);
+	}
+
+}
