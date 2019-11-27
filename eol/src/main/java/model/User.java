359f127dad47210ab263598cbf1712aa53c1865b
CaioCresencio <caiocresencio98@gmail.com>
2019-11-27 11:04:38 -0300

    Criação das pastas

---
+++ eol/src/main/java/model/User.java
@@ -0,0 +1,23 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String password;
+
+
+
+
+}
