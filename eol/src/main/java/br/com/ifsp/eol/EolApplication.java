ecdf4e9480bc1819305f0e2322ed6a6e4d87fe37
GabrielBergamim <gabriel.bergamim@outlook.com>
2019-11-27 22:18:11 -0300

    User, Location e Roles model

--- eol/src/main/java/br/com/ifsp/eol/EolApplication.java
+++ eol/src/main/java/br/com/ifsp/eol/EolApplication.java
@@ -1,13 +1,22 @@
 package br.com.ifsp.eol;
 
+import br.com.ifsp.eol.model.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EolApplication {
+public class EolApplication implements CommandLineRunner {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EolApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		User user = new User("Gabriel", "biel.muitoloko@gmail.com", "123456","175673", "123");
+		//userRepository.save(user);
+	}
 }
--- eol/src/main/java/br/com/ifsp/eol/config/WebConfig.java
+++ eol/src/main/java/br/com/ifsp/eol/config/WebConfig.java
@@ -1,4 +1,4 @@
-package config;
+package br.com.ifsp.eol.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
